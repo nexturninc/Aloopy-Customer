c3d52a7a273534d1ab67942f68183ab813e3878a
michael_c <michael.calingo@nexturninc.com>
2015-09-15 13:27:12 +0800

    Offline Mode for Customer Profile

---
+++ AloopyCustomerApp/app/src/main/java/com/customer/aloopy/aloopydatabase/CustomerCouponContract.java
@@ -0,0 +1,51 @@
+package com.customer.aloopy.aloopydatabase;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by imbisibol on 9/15/2015.
+ */
+public final class CustomerCouponContract {
+
+    public CustomerCouponContract() {
+    }
+
+    private static final String TEXT_TYPE = " TEXT";
+    private static final String DATE_TYPE = " DATETIME";
+    private static final String COMMA_SEP = ",";
+    public static final String SQL_CREATE_TABLE =
+            "CREATE TABLE " + CustomerCouponInformation.TABLE_NAME + " (" +
+                    CustomerCouponInformation._ID + " INTEGER PRIMARY KEY," +
+                    CustomerCouponInformation.COLUMN_NAME_Customer_Coupon_ID + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Coupon_ID + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Coupon_Name + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Start_Date + DATE_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_End_Date + DATE_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Store_Id + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Store_Coupon_Id + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Main_Banner_Image + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_QR_Code_Image + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Claimed + TEXT_TYPE + COMMA_SEP +
+                    CustomerCouponInformation.COLUMN_NAME_Date_Created + TEXT_TYPE +
+                    " )";
+    public static final String SQL_DELETE_TABLE =
+            "DROP TABLE IF EXISTS " + CustomerCouponInformation.TABLE_NAME;
+
+    /* Inner class that defines the table contents */
+    public static abstract class CustomerCouponInformation implements BaseColumns {
+        public static final String TABLE_NAME = "CustomerCouponInformation";
+        public static final String COLUMN_NAME_Customer_Coupon_ID = "CustomerCouponId";
+        public static final String COLUMN_NAME_Coupon_ID = "CouponId";
+        public static final String COLUMN_NAME_Coupon_Name = "CouponName";
+        public static final String COLUMN_NAME_Start_Date = "StartDate";
+        public static final String COLUMN_NAME_End_Date = "EndDate";
+        public static final String COLUMN_NAME_Store_Id = "StoreId";
+        public static final String COLUMN_NAME_Store_Coupon_Id = "StoreCouponId";
+        public static final String COLUMN_NAME_Main_Banner_Image = "MainBannerImage";
+        public static final String COLUMN_NAME_QR_Code_Image = "QRCodeImage";
+        public static final String COLUMN_NAME_Claimed = "Claimed";
+        public static final String COLUMN_NAME_Date_Created = "DateCreated";
+
+    }
+}
+
